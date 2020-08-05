cb8635f405c554067d2922578ff02f17a4bae3e1
avashisht28 <36313995+avashisht28@users.noreply.github.com>
2020-08-04 22:30:37 -0400

    Update FirstJavaProgram.java

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,5 @@
 public class FirstJavaProgram {
   public static void main(String[] args){
-    System.out.println("This is my first program in java");
-  }//End of main
+    System.out.println("This is my first program in java... add new changes here");
+  }//End of mainm
 }//End of FirstJavaProgram Class
